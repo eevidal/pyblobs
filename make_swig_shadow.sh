aa498e844c27a8d48bf1f54b389e7d17d894654e
kostmo <kostmo@97929fb4-1155-0410-ab90-53e0aa93e185>
2008-08-23 20:14:59 +0000

    Added 64-bit CPU detection to compile script

--- make_swig_shadow.sh
+++ make_swig_shadow.sh
@@ -2,7 +2,13 @@
 
 swig -c++ -python -shadow BlobResult.i
 swig -c++ -python -shadow Blob.i
-gcc -c -fPIC BlobResult.cpp BlobResult_wrap.cxx BlobExtraction.cpp Blob.cpp Blob_wrap.cxx -I/usr/include/python2.5 `pkg-config --cflags opencv`
+
+if [ $HOSTTYPE == "x86_64" ]; then
+	gcc -c -fPIC BlobResult.cpp BlobResult_wrap.cxx BlobExtraction.cpp Blob.cpp Blob_wrap.cxx -I/usr/include/python2.5 `pkg-config --cflags opencv`
+else
+	gcc -c BlobResult.cpp BlobResult_wrap.cxx BlobExtraction.cpp Blob.cpp Blob_wrap.cxx -I/usr/include/python2.5 `pkg-config --cflags opencv`
+fi
+
 ld -shared Blob.o Blob_wrap.o -o _Blob.so `pkg-config --libs opencv`
 ld -shared BlobResult.o BlobResult_wrap.o BlobExtraction.o Blob.o -o _BlobResult.so `pkg-config --libs opencv`
 
