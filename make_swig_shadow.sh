4ba2fe4f776a9dceefea3f47ad3fb88ca53605f3
kostmo <kostmo@97929fb4-1155-0410-ab90-53e0aa93e185>
2008-08-23 20:18:54 +0000

    Added 64-bit acknowledgement echo to compile script

--- make_swig_shadow.sh
+++ make_swig_shadow.sh
@@ -4,8 +4,10 @@ swig -c++ -python -shadow BlobResult.i
 swig -c++ -python -shadow Blob.i
 
 if [ $HOSTTYPE == "x86_64" ]; then
+	echo "Compiling for 64-bit."
 	gcc -c -fPIC BlobResult.cpp BlobResult_wrap.cxx BlobExtraction.cpp Blob.cpp Blob_wrap.cxx -I/usr/include/python2.5 `pkg-config --cflags opencv`
 else
+	echo "You are not 64-bit."
 	gcc -c BlobResult.cpp BlobResult_wrap.cxx BlobExtraction.cpp Blob.cpp Blob_wrap.cxx -I/usr/include/python2.5 `pkg-config --cflags opencv`
 fi
 
